7494ee6f06fb27de61ef473d7b946fcc7e56cbad
RonyAgungAriBudiman <32225565+RonyAgungAriBudiman@users.noreply.github.com>
2024-08-06 09:07:15 +0700

    -

--- signin.php
+++ signin.php
@@ -46,7 +46,7 @@ if (isset($_POST["signin"])) {
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>GSR RT02</title>
+  <title>TOKO MEUBLE</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -74,7 +74,7 @@ if (isset($_POST["signin"])) {
 <body class="hold-transition login-page">
   <div class="login-box">
     <div class="login-logo">
-      <a href="index.php"><b>GSR RT02</b></a>
+      <a href="index.php"><b>TOKO MEUBLE</b></a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
